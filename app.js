f3c980af8b69830b8b2c12ce7c93775ef0d9dbf4
nuhakhangr8 <nuhakhangr8@gmail.com>
2021-08-26 09:47:57 +0530

    fix: function flow

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 var dateOfBirth=document.querySelector("#date-of-birth");
 var luckyNumber=document.querySelector("#lucky-number");
 var checkButton=document.querySelector("#check-button");
+var outputBox=document.querySelector(".output-box")
 
-checkButton.addEventListener("click",function checkluckyNumberanddate(){
-    console.log(dateOfBirth.value)
-console.log(luckyNumber.value)
-})
+//You can also sum the digits using while loop using modulus
+// Extra:Read about how to traverse index and values using in and of in for loop.
+
+
+function compareValues(sum,luckyNumber){
+    if(sum%luckyNumber===0){
+        outputBox.innerText="your Birthday is Lucky🤩"
+        }else{
+        outputBox.innerText="Your Birthday is not lucky 😬 "
+        }
+}
+
+function checkIfBirthdayLucky(){
+    //we are calling both the functions from here
+    var dob=dateOfBirth.value;
+    var sum=calculateSumOfDigitsofdate(dob)
+    if(sum&&dob)
+    compareValues(sum,luckyNumber.value);
+    else{
+        outputBox.innerText="Enter both values 🙄";
+    }
+}
+
+function calculateSumOfDigitsofdate(dob){
+    dob=dob.replaceAll("-","");
+    let sum=0;
+    for(let i=0;i<dob.length;i++){
+        sum= sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+
+checkButton.addEventListener("click",checkIfBirthdayLucky)
