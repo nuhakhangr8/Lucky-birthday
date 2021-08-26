48ee40cb0dcda3e38f09c88ad720326f344e8c26
nuhakhangr8 <nuhakhangr8@gmail.com>
2021-08-26 08:44:47 +0530

    feat: getting components from html land to js land

---
+++ app.js
@@ -0,0 +1,8 @@
+var dateOfBirth=document.querySelector("#date-of-birth");
+var luckyNumber=document.querySelector("#lucky-number");
+var checkButton=document.querySelector("#check-button");
+
+checkButton.addEventListener("click",function checkluckyNumberanddate(){
+    console.log(dateOfBirth.value)
+console.log(luckyNumber.value)
+})
